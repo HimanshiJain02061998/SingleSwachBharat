580de605f412bcb956e551d241dcb16d49cbde3d
Biswajit <biswajitsingh780@gmail.com>
2025-12-22 17:50:21 +0530

    Work history offline done

---
+++ kotlinsbalibrary/src/main/java/com/appynitty/kotlinsbalibrary/ghantagadi/dao/WorkHistoryDetailsDao.kt
@@ -0,0 +1,48 @@
+package com.appynitty.kotlinsbalibrary.ghantagadi.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.appynitty.kotlinsbalibrary.ghantagadi.model.WorkHistoryDetailsData
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WorkHistoryDetailsDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWorkHistoryDetails(workHistoryDetailsData: WorkHistoryDetailsData)
+
+    @Query("SELECT * FROM work_history_details_table")
+    fun getWorkHistoryDetails(): LiveData<List<WorkHistoryDetailsData>>
+
+    @Query("DELETE FROM work_history_details_table")
+    suspend fun deleteAllWorkHistoryDetails()
+
+    @Query("""
+    SELECT *
+    FROM work_history_details_table
+    WHERE date = :date
+    ORDER BY time DESC
+""")
+    fun getWorkHistoryDetailsByDate(date: String): Flow<List<WorkHistoryDetailsData>>
+
+    @Query("""
+    SELECT COUNT(*)
+    FROM work_history_details_table
+    WHERE date = :date
+""")
+    fun getWorkHistoryDetailsCountForDate(date: String):Int
+
+    @Query("""
+    DELETE FROM work_history_details_table
+    WHERE date = :date AND Refid = :refId
+""")
+    suspend fun deleteByDateAndRefId(
+        date: String,
+        refId: String?
+    )
+
+
+}
